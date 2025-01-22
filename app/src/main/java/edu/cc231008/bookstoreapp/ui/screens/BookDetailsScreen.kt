070f768d0a452ac5b8c08898b59b1ebeb7fd1470
SelinaHacker <cc231072@fhstp.ac.at>
2025-01-22 16:16:11 +0100

    UI fixes

--- app/src/main/java/edu/cc231008/bookstoreapp/ui/screens/BookDetailsScreen.kt
+++ app/src/main/java/edu/cc231008/bookstoreapp/ui/screens/BookDetailsScreen.kt
@@ -12,6 +12,7 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.imePadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
@@ -97,6 +98,7 @@ fun BookDetailsScreen(
             .fillMaxSize()
             .background(Color(0xFFF5F5DC))
             .verticalScroll(scrollState)
+            .imePadding()
     ) {
         // Header
         Box(
